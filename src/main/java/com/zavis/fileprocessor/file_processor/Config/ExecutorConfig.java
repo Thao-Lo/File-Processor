073fa61b06a509d556fbdaa82abac533f90990ce
Zavis <thezavis@gmail.com>
2025-02-08 02:48:57 +1100

    Config ExecutorConfig to create fixed thread pool 4 threads

--- src/main/java/com/zavis/fileprocessor/file_processor/Config/ExecutorConfig.java
+++ src/main/java/com/zavis/fileprocessor/file_processor/Config/ExecutorConfig.java
@@ -1,5 +1,31 @@
 package com.zavis.fileprocessor.file_processor.Config;
 
-public class ExecutorConfig {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExecutorConfig {
+	@Bean
+	public ExecutorService executorService() {		
+		return Executors.newFixedThreadPool(4); //to process 4 files asynchonous, more than 4 will be move to queue
+	}	
 }
+
+//@Configuration
+//@EnableAsync
+//public class ExecutorConfig {
+//
+//	@Bean(name = "asyncExecutor")
+//	public TaskExecutor asyncExecutor() {		
+//		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//		executor.setCorePoolSize(4);
+//		executor.setMaxPoolSize(5);
+//		executor.setQueueCapacity(15);
+//		executor.setThreadNamePrefix("AsyncExecutor-");
+//		executor.initialize();
+//		return executor;
+//	}
+//}
