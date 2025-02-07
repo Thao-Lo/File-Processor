919db3950eb7994cc35c916573265a2e9d020f67
Zavis <thezavis@gmail.com>
2025-02-08 02:50:33 +1100

    Service FactoryService, create interface class with method to process file

--- src/main/java/com/zavis/fileprocessor/file_processor/Service/FileProcessorService.java
+++ src/main/java/com/zavis/fileprocessor/file_processor/Service/FileProcessorService.java
@@ -1,5 +1,21 @@
 package com.zavis.fileprocessor.file_processor.Service;
 
-public class FileProcessorService {
+import java.util.concurrent.ExecutorService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zavis.fileprocessor.file_processor.Factory.FileProcessorFactory;
+import com.zavis.fileprocessor.file_processor.Processor.FileProcessor;
+
+public class FileProcessorService {
+	@Autowired
+	private ExecutorService executor;
+	
+	public void processFile(String filePath) {
+		//pick a free thread to process a file, if not will be place in queue
+		executor.submit(() -> {
+			FileProcessor processor = FileProcessorFactory.getProcessor(filePath);
+			processor.process(filePath);
+		});
+	}
 }
