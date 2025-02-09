cb420ccfed61a926ac6f74a3fad73b99a2b7257a
Zavis <thezavis@gmail.com>
2025-02-10 01:51:31 +1100

    Service use autowired for FileProcessorFactory and fix logic

--- src/main/java/com/zavis/fileprocessor/file_processor/Service/FileProcessorService.java
+++ src/main/java/com/zavis/fileprocessor/file_processor/Service/FileProcessorService.java
@@ -3,19 +3,21 @@ package com.zavis.fileprocessor.file_processor.Service;
 import java.util.concurrent.ExecutorService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.zavis.fileprocessor.file_processor.Factory.FileProcessorFactory;
-import com.zavis.fileprocessor.file_processor.Processor.FileProcessor;
 
+@Service
 public class FileProcessorService {
 	@Autowired
 	private ExecutorService executor;
+	@Autowired 
+	FileProcessorFactory fileProcessorFactory;
 	
 	public void processFile(String filePath) {
 		//pick a free thread to process a file, if not will be place in queue
 		executor.submit(() -> {
-			FileProcessor processor = FileProcessorFactory.getProcessor(filePath);
-			processor.process(filePath);
+			fileProcessorFactory.getProcessor(filePath).process(filePath);			
 		});
 	}
 }
