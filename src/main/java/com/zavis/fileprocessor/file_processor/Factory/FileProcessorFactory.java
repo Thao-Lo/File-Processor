d9aa64d17bcb02c1fa24a8d8c06164da8fc75343
Zavis <thezavis@gmail.com>
2025-02-09 23:42:49 +1100

    FactoryPatter apply factory pattern in returning new object depends on file type: csv, json or xml

--- src/main/java/com/zavis/fileprocessor/file_processor/Factory/FileProcessorFactory.java
+++ src/main/java/com/zavis/fileprocessor/file_processor/Factory/FileProcessorFactory.java
@@ -1,5 +1,22 @@
 package com.zavis.fileprocessor.file_processor.Factory;
 
+import com.zavis.fileprocessor.file_processor.Processor.CSVProcessor;
+import com.zavis.fileprocessor.file_processor.Processor.FileProcessor;
+import com.zavis.fileprocessor.file_processor.Processor.JSONProcessor;
+import com.zavis.fileprocessor.file_processor.Processor.XMLProcessor;
+
 public class FileProcessorFactory {
 
+	// factory pattern
+	public static FileProcessor getProcessor(String filePath) { // order1.csv, order2.json, order3.xml
+		if (filePath.endsWith(".csv")) {
+			return new CSVProcessor();
+		} else if (filePath.endsWith(".json")) {
+			return new JSONProcessor();
+		} else if (filePath.endsWith(".xml")) {
+			return new XMLProcessor();
+		} else {
+			throw new IllegalArgumentException("Unsupported file type: " + filePath);
+		}
+	}
 }
